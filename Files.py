0008c90871beb3ce98dec1e17ef4072f406b82b7
Kaique Batista Ramos <108313417+kaique-smaug@users.noreply.github.com>
2024-11-27 09:54:21 -0300

    Update Manipulation of Files

--- Files.py
+++ Files.py
@@ -1,10 +1,12 @@
-__version__ = '1.1.2'
+__version__ = '1.1.5'
 """
     Import all libraries bibliotecas what i am using at the script
 """
-import shutil
+from os import makedirs, listdir, rename, remove,  O_WRONLY, O_CREAT, fdopen, open
+from os.path import join, exists, splitext, isfile, isdir, basename
+from shutil import copy, rmtree, move
+from codecs import open as open_dcs
 import time
-import os
 
 class Files:
     def __init__(self):
@@ -14,17 +16,17 @@ class Files:
         Copy any file
     '''
     def copy(self, path: str = None, file: str = None, destiny: str = None) -> None:
-        self._pathFileComplet = os.path.join(path, file)
-        shutil.copy(self._pathFileComplet, destiny)
+        self._pathFileComplet = join(path, file)
+        copy(self._pathFileComplet, destiny)
 
     '''
         create a new folder from  of specifications what past and copy at the a place for other
     '''
     def craeteForlderAndCopy(self, name_folder: str = None, path : str = None, file: str = None, destiny: str = None) -> None:
-        self._nameFolder = name_folder
+        self.__nameFolder = name_folder
 
-        if not os.path.exists(name_folder):
-            os.makedirs(self._nameFolder, exist_ok=True)
+        if not exists(name_folder):
+            makedirs(self.__nameFolder, exist_ok=True)
             self.copy(path, file, destiny)
 
         else:
@@ -34,8 +36,8 @@ class Files:
         Create folder from especigications 
     '''
     def create(self, name_folder: str = None):
-        if not os.path.exists(name_folder):
-            os.makedirs(name_folder, exist_ok=True)
+        if not exists(name_folder):
+            makedirs(name_folder, exist_ok=True)
 
     '''
         Rename alls files whet have at folfer
@@ -44,17 +46,20 @@ class Files:
     '''
     def renameFiles(self, path: str = None, files: str = None, extension: str = None) -> None:
 
-        itens = os.listdir(path)
+        itens = listdir(path)
 
         for i in range(len(itens)):
-            self._folder = os.path.join(path, itens[i])
-            self._renameFile = os.path.join(path, files[i])
+            self._folder = join(path, itens[i])
+            self._renameFile = join(path, files[i])
 
-            self._name, self._extension = os.path.splitext(itens[i]) 
+            self._name, self._extension = splitext(itens[i]) 
 
             if self._extension != extension:
-                print('if')
-                os.rename(self._folder, f'{self._renameFile.replace(f"{self._extension}", "")}.{extension}')
+                rename(self._folder, f'{self._renameFile.replace(f"{self._extension}", "")}.{extension}')
+
+    def rename_one_file(self, path, nameOld, nameNow):
+       if exists(path):
+          rename(nameOld, nameNow)
 
     '''
         Check if files is at folder if not have wait teh download or something like that
@@ -64,31 +69,47 @@ class Files:
         while self._check:
             time.sleep(2)
             try:
-                for file_name in os.listdir(directory):
+                for file_name in listdir(directory):
                     if file_name.endswith(extension):
                         self._check = False
             except:
                 self._check = True
         
     def delete(self, path) -> None:
-        if os.path.exists(path):
-            if os.path.isfile(path):
-                os.remove(path)  # Remove the file
-            elif os.path.isdir(path):
-                shutil.rmtree(path)  # Remove the directory and its contents
+        if exists(path):
+            if isfile(path):
+                remove(path)  # Remove the file
+            elif isdir(path):
+                rmtree(path)  # Remove the directory and its contents
     
     def move_file(self, nameFile, destinyFile) -> None:
-            self._destination_path = os.path.join(destinyFile, os.path.basename(nameFile))
-            shutil.move(nameFile, self._destination_path)
-                
+            self._destination_path = join(destinyFile, basename(nameFile))
+            move(nameFile, self._destination_path)
+
     def create_text(self, nameFile) -> None:
         self._nameFile = nameFile
 
-        with os.fdopen(os.open(self._nameFile, os.O_WRONLY | os.O_CREAT), 'wb') as file:
+        with fdopen(open(self._nameFile, O_WRONLY | O_CREAT), 'wb') as file:
             pass
             
     def convert_file(self, nameFile, newNameFile) -> None:
         self._filetxt = nameFile
         self._fileBat = newNameFile
 
-        os.rename(self._filetxt, self._fileBat)
+        rename(self._filetxt, self._fileBat)
+    
+    # Method to adjust the content of a file
+    def adjust_bar(self, path: str = None, path_destiny: str = None, old_replace: str = None, new_replace: str = None):
+        self._list = []
+        # Opens the specified file for reading and preparing to write
+        with open_dcs(path, 'r+', encoding='utf-8') as f:
+            # Reads the content of the file
+            self._content = f.read()
+            # Replaces triple backslashes with double backslashes in the content
+            self._content = self._list.append(self._content.replace(f'{old_replace}', f'{new_replace}'))
+
+        # Opens the destination file for appending
+        with open_dcs(path_destiny, 'a', encoding='utf-8') as f:
+            # Writes each line from the list to the destination file
+            for line in self._list:
+                f.write(line)
